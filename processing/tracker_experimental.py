dbd80be398b5d60abfbd44815e2034b60d2ecf45
FotiosBistas <fotis.bistas@gmail.com>
2025-01-07 14:31:11 +0200

    Labels for objects
    Signed-off-by: FotiosBistas <fotis.bistas@gmail.com>

--- processing/tracker_experimental.py
+++ processing/tracker_experimental.py
@@ -2,6 +2,7 @@ import json
 import cv2
 import logging
 import numpy as np
+import time
 
 from object_detection import ObjectDetection
 
@@ -19,7 +20,6 @@ else:
         format="%(asctime)s - %(levelname)s - %(message)s",  # Customize the format
         datefmt="%Y-%m-%d %H:%M:%S"  # Add a timestamp for better readability
     )
-
 
 class ObjectTracker:
     def __init__(self, green_line_indices, min_positions_detected: int = 3 , should_visualize: bool = False):
@@ -39,7 +39,7 @@ class ObjectTracker:
         self.classes = {2: "car", 7: "truck"}
         self.frame_count = 0
 
-        self.SCORE_THRESHOLD = 0.6
+        self.SCORE_THRESHOLD = 0.5
 
     def write_json_file(self, json_path: str):
         tracking_data = {}
@@ -60,12 +60,12 @@ class ObjectTracker:
         for object_id, positions in self.tracking_objects.items():
             last_position = positions[-1]
             x, y = last_position
-            #vehicle_type = self.vehicle_types[object_id]
+            vehicle_type = self.vehicle_types[object_id]
 
             w, h = 50, 50
             cv2.rectangle(frame, (x - w // 2, y - h // 2), (x + w // 2, y + h // 2), (0, 255, 0), 2)
 
-            label = f"ID: {object_id}"
+            label = f"ID: {object_id} {vehicle_type}"
             cv2.putText(frame, label, (x - w // 2, y - h // 2 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
         cv2.imshow("Object Tracking", frame)
@@ -74,62 +74,77 @@ class ObjectTracker:
         self, 
         center_points_x: np.ndarray, 
         center_points_y: np.ndarray,
+        class_ids: np.ndarray,
     ): 
 
 
         if len(self.tracking_objects) == 0:  
             # Assign new IDs to unmatched detections
-            for position_x, position_y in zip(center_points_x, center_points_y):
+            for i, (position_x, position_y) in enumerate(zip(center_points_x, center_points_y)):
                 self.tracking_objects[self.track_id] = [np.array((position_x, position_y))]
+
+                detected_class_id = class_ids[i]
+                if detected_class_id in self.classes:
+                    self.vehicle_types[self.track_id] = self.classes[detected_class_id]
+                else:
+                    self.vehicle_types[self.track_id] = "unknown"  # Default for unidentified classes
+
                 self.track_id += 1
             return
 
         updated_tracking_objects = {}
         matched_objects = [False] * len(center_points_x)
 
+        current_centers = np.column_stack((center_points_x, center_points_y))
+
         for object_id, positions in self.tracking_objects.items(): 
 
             # Get the last known position of the object
             # Try to find a best match of the object in the current frame
-            last_position = positions[-1]
+            last_position = positions
             best_match = None
 
-            current_centers = np.column_stack((center_points_x, center_points_y))
             distances = np.linalg.norm(current_centers - last_position, axis=1)
 
             # Find the closest point within the threshold
             min_distance = np.min(distances)
-            best_match = np.argmin(distances) if min_distance < 50 else None
+            best_match = np.argmin(distances) if min_distance < 20 else None
 
             if best_match is not None:
                 # Append the position of the tracked object as a new position
-                updated_tracking_objects[object_id] = positions + [np.array((center_points_x[best_match], center_points_y[best_match]))]
+                #updated_tracking_objects[object_id] = positions + [np.array((center_points_x[best_match], center_points_y[best_match]))]
+                updated_tracking_objects[object_id] = [np.array((center_points_x[best_match], center_points_y[best_match]))]
                 matched_objects[best_match] = True
 
-        non_matched_centers = [np.array((center_x, center_y)) for (center_x, center_y), matched in zip(zip(center_points_x, center_points_y), matched_objects) if not matched] 
         # Assign new IDs to unmatched detections
-        for arr in non_matched_centers:
-            updated_tracking_objects[self.track_id] = [arr]
-            self.track_id += 1
+        for i, matched in enumerate(matched_objects):
+            if not matched:
+                updated_tracking_objects[self.track_id] = [np.array((center_points_x[i], center_points_y[i]))]
+                # Assign vehicle type based on class_ids
+                detected_class_id = class_ids[i]
+                if detected_class_id in self.classes:
+                    self.vehicle_types[self.track_id] = self.classes[detected_class_id]
+                else:
+                    self.vehicle_types[self.track_id] = "unknown"  # Default for unidentified classes
+
+                self.track_id = (self.track_id + 1) % 200
 
         self.tracking_objects = updated_tracking_objects
 
     def detect_position(
         self, 
-        class_ids: np.ndarray, 
         scores: np.ndarray, 
         boxes: np.ndarray
     ) -> tuple:
         """
         Detects the center positions of objects in the current frame.
-        :param class_ids: Array of detected class IDs
         :param scores: Array of confidence scores
         :param boxes: Array of bounding box coordinates
         :return: A list of center points and their corresponding class IDs
         """
 
-        center_points_x = np.zeros(class_ids.shape[0], dtype=np.int16)
-        center_points_y = np.zeros(class_ids.shape[0], dtype=np.int16)
+        center_points_x = np.zeros(scores.shape[0], dtype=np.int16)
+        center_points_y = np.zeros(scores.shape[0], dtype=np.int16)
 
         for i, box in enumerate(boxes):
 
@@ -137,8 +152,8 @@ class ObjectTracker:
                 continue
 
             x, y, w, h = box  
-            center_x = int(x + w / 2)
-            center_y = int(y + h / 2)
+            center_x = x + w / 2
+            center_y = y + h / 2
 
             center_points_x[i] = center_x
             center_points_y[i] = center_y
@@ -148,7 +163,8 @@ class ObjectTracker:
         return center_points_x, center_points_y
 
     def process_video(self, video_path: str):
-
+        
+        timer = cv2.TickMeter()
         self.cap = cv2.VideoCapture(video_path)
 
         while True:
@@ -167,12 +183,17 @@ class ObjectTracker:
 
             class_ids, scores, boxes = self.od.detect(frame)
 
-            center_points_x, center_points_y = self.detect_position(class_ids, scores, boxes)
+            center_points_x, center_points_y = self.detect_position(scores, boxes)
 
+            start_time = time.time()
             self.match_objects(
                 center_points_x=center_points_x,
                 center_points_y=center_points_y,
+                class_ids=class_ids,
             )
+            timer.stop()
+            # Print or log inference time
+            logging.debug(f"Inference Time: {time.time() - start_time} s")
 
 
             if self.should_visualize:
