878a5b8e749a85a249d411c3c189c88626abef37
FotiosBistas <fotis.bistas@gmail.com>
2025-01-20 21:59:01 +0200

    Seems to work faster
    Signed-off-by: FotiosBistas <fotis.bistas@gmail.com>

--- processing/function_app.py
+++ processing/function_app.py
@@ -2,11 +2,13 @@ import logging
 import tempfile
 import os
 import azure.functions as func
+from azure.storage.blob import BlobServiceClient
+
 
 from tracking_pkg import Tracker
 
 # Use a global tracker
-tracker = Tracker(should_visualize=True)
+tracker = Tracker(should_visualize=False)
 
 app = func.FunctionApp()
 
@@ -15,6 +17,11 @@ app = func.FunctionApp()
 def video_processing(myblob: func.InputStream):
     logging.info(f"Object detection triggered for blob: {myblob.name}")
 
+    data = myblob.read()
+    blob_size = len(data)
+
+    logging.info(f"Read blob of size {blob_size}")
+
     temp_file_path = tempfile.gettempdir()
     logging.info(f"Found {temp_file_path} directory")
     # Extract just the file name from the blob name
@@ -22,8 +29,16 @@ def video_processing(myblob: func.InputStream):
     temp_video_path = f"{temp_file_path}/{blob_filename}"
     logging.info(f"Temp video path: {temp_video_path}")
 
-    with open(temp_video_path, "wb") as f:
-        f.write(myblob.read())
+    blob_service_client = BlobServiceClient.from_connection_string(os.getenv("AzureWebJobsStorage"))
+     # Get the container client
+    container_client = blob_service_client.get_container_client("input-segments-container")
+
+    # Get the blob client for the specified blob
+    blob_client = container_client.get_blob_client(os.path.basename(myblob.name))
+
+    # Download the blob to a local file
+    with open(temp_video_path, "wb") as file:
+        file.write(blob_client.download_blob().readall())
     
     tracker(video_path=temp_video_path)
 
